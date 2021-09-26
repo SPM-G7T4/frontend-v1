70f6603b01854d1dc820869f619bc20cfef847f3
joenchua <60461285+joenchua@users.noreply.github.com>
2021-09-27 01:40:24 +0800

    feat: create view all courses page

---
+++ src/pages/Courses/CoursesEnrolled.js
@@ -0,0 +1,18 @@
+export const CoursesEnrolled = [
+  {
+    url: "#",
+    title: "Printing Coach Repairs",
+    badge: "Badge Name",
+    date: "5 Oct - 20 Oct",
+    prerequisites: "No Prerequisities",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    url: "#",
+    title: "General Repairs",
+    badge: "Badge Name",
+    date: "5 Oct - 20 Oct",
+    prerequisites: "No Prerequisities",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+];
