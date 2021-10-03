545e0a96d3598ed4c457839951520b1d24ff31f0
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2021-10-03 22:52:14 +0800

    feat: add dynamic course details page (#8)

--- src/components/Courses/CourseTab.js
+++ src/components/Courses/CourseTab.js
@@ -2,19 +2,21 @@ import React from "react";
 
 import styles from "./Courses.module.scss";
 
-import { Link } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 
 import Tab from "react-bootstrap/Tab";
 
-
 const CourseTab = (props) => {
+
+  const { url } = useRouteMatch();
+
   return (
     <Tab.Pane eventKey={props.tab}>
       <div className={styles.listing}>
         {props.courseType.map((value, key) => {
           return (
             <div key={key} className={styles["listing-item"]}>
-              <Link to="">
+              <Link to={`${url}/${value.course_id}`}>
                 <div className={styles["title"]}>{value.title}</div>
                 <div className={styles["subtitle"]}>
                   <span>{value.badge}</span>
