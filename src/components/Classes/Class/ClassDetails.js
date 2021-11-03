db6f28e3ef0edc5bc97dde54809113197919ecb1
seanisoverhere <sean_choon@hotmail.com>
2021-11-04 02:15:02 +0800

    feat: add learner view enrolled classes page

--- src/components/Classes/Class/ClassDetails.js
+++ src/components/Classes/Class/ClassDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 import styles from "./ClassDetails.module.scss";
 
@@ -13,6 +13,7 @@ const ClassDetails = () => {
   const { courseId, classId } = useParams();
 
   const course = ClassDetailsAll.find((p) => p.course_id === courseId);
+  const userType = sessionStorage.getItem("auth-token");
 
   if (!course) {
     return (
@@ -28,44 +29,71 @@ const ClassDetails = () => {
         <Container fluid>
           <div className={styles.header}>
             <div className="text-primary">Class {classId}</div>
-            <div className={styles["title"]}>{course.course_id}: {course.course_name}</div>
+            <div className={styles["title"]}>
+              {course.course_id}: {course.course_name}
+            </div>
             <div className="pb-3">{course.desc}</div>
           </div>
           <div className={styles.sections}>
             {course.sections.map((el) => {
               return (
-                <div key={el.name} className={styles['section-item']}>
+                <div key={el.name} className={styles["section-item"]}>
                   <ul>
                     <li>
                       <div className="text-secondary">Section</div>
-                      <div className={styles['section-name']}>{el.name}</div>
+                      <div className={styles["section-name"]}>{el.name}</div>
                       <div className="text-secondary">{el.description}</div>
                     </li>
                     <Accordion>
                       <Accordion.Item eventKey="0" className="materials">
-                        <Accordion.Header>View Course Materials</Accordion.Header>
+                        <Accordion.Header>
+                          View Course Materials
+                        </Accordion.Header>
                         <Accordion.Body>
                           {el.materials.map((el) => {
                             return (
-                              <div className={styles['material-item']} key={el.title}>
+                              <div
+                                className={styles["material-item"]}
+                                key={el.title}
+                              >
                                 <span>{el.title}</span>
                                 <span>
-                                  <a href={el.view} target="_blank" rel="noreferrer" className="btn btn-primary" style={{marginRight: '10px'}}>View</a>
-                                  <a href={el.download} className="btn btn-primary">Download</a>
+                                  <a
+                                    href={el.view}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="btn btn-primary"
+                                    style={{ marginRight: "10px" }}
+                                  >
+                                    View
+                                  </a>
+                                  <a
+                                    href={el.download}
+                                    className="btn btn-primary"
+                                  >
+                                    Download
+                                  </a>
                                 </span>
                               </div>
-                            )
+                            );
                           })}
                         </Accordion.Body>
                       </Accordion.Item>
                       <Accordion.Item eventKey="1" className="materials">
-                        <Accordion.Header>View Created Quizzes</Accordion.Header>
+                        <Accordion.Header>
+                          View Created Quizzes
+                        </Accordion.Header>
                         <Accordion.Body>
-                          <div>
-                            <a href="/quizzes" className="w-100 mb-3 btn btn-primary">
-                              Create Quiz
-                            </a>
-                          </div>
+                          {userType === "trainer" ? (
+                            <div>
+                              <Link
+                                to="/quizzes"
+                                className="w-100 mb-3 btn btn-primary"
+                              >
+                                Create Quiz
+                              </Link>
+                            </div>
+                          ) : null}
                           <div>
                             {el.quiz.length > 0 ? "" : "No quizzes created "}
                           </div>
@@ -74,7 +102,7 @@ const ClassDetails = () => {
                     </Accordion>
                   </ul>
                 </div>
-              )
+              );
             })}
           </div>
         </Container>
