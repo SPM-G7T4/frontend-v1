7a6b990beea17fe2a772f4c73b25c3b9be3b65d0
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2021-11-09 15:45:40 +0800

    chore: fix typos (#32)

--- src/components/Classes/Class/ClassDetailsAll.js
+++ src/components/Classes/Class/ClassDetailsAll.js
@@ -92,4 +92,48 @@ export const ClassDetailsAll = [
       }
     ]
   },
+  {
+    course_id: "REP1301",
+    course_name: "Printer Operations",
+    class_id: 1,
+    desc: "Briefly describes the relationship between printers, and their assigned lines, processes and statuses. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    sections: [
+      {
+        name: "Basic Troubleshooting",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        materials: [
+          {
+            title: "01 Basic Troubleshooting - Slides", 
+            view: "https://docs.google.com/presentation/d/e/2PACX-1vTEq9-h_nAuKfizis_bJFlAVbB6UsJaOBOhQjDcyjVwS_9VbC_AaN-H20efB2IBWA/pub?start=false&loop=false&delayms=3000", 
+            download: "https://spm-g7t4.s3.amazonaws.com/All+in+One+-+Basic+Troubleshooting.pptx"
+          },
+        ],
+        quiz: []
+      },
+      {
+        name: "Advanced Troubleshooting",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        materials: [
+          {
+            title: "02 Advanced Troubleshooting - Slides",
+            view: "https://docs.google.com/presentation/d/e/2PACX-1vRXmX46z12RY0FYPS2m8soG7TT-oCT1ryKjl08Oq2lFxtTZIeN6X1iCU9oUPNMjsQ/pub?start=false&loop=false&delayms=3000",
+            download: "https://spm-g7t4.s3.amazonaws.com/All+in+One+-+Advanced+Troubleshooting.pptx"
+          },
+        ],
+        quiz: []
+      },
+      {
+        name: "Printer Settings",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        materials: [
+          {
+            title: "03 Printer Settings - Slides",
+            view: "https://docs.google.com/presentation/d/e/2PACX-1vQE0vnSm_rabpem5muD4hwHJx_FNIAipQ9JP6m4KwbTCE3p4NAprinDhTyC7HYysQ/pub?start=false&loop=false&delayms=3000",
+            download: "https://spm-g7t4.s3.amazonaws.com/All+in+One+-+Printer+Settings.pptx"
+          },
+        ],
+        quiz: []
+      }
+    ]
+  },
 ];
